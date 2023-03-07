16753fb63f217b99f2ba719d6c463c67f7609121
Diego Salazar <dsalazar@ec.krugercorp.com>
2023-03-07 14:16:35 -0500

    [Update] GET API Unit Test

--- src/test/java/controller/BookControllerTest.java
+++ src/test/java/controller/BookControllerTest.java
@@ -4,17 +4,31 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import entity.Book;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import repository.BookRepository;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(MockitoJUnitRunner.class)
 class BookControllerTest {
@@ -27,7 +41,7 @@ class BookControllerTest {
     @Mock
     private BookRepository bookRepository;
 
-    @InjectMocks
+   @InjectMocks
     private BookController bookController;
 
     Book RECORD_1 = new Book(1L, "Atomic Habits", "How to build better habits", 5);
@@ -35,12 +49,27 @@ class BookControllerTest {
     Book RECORD_3 = new Book(3L, "Grokking Algoritms", "Learn algoritms the fun way", 5);
 
 
-    @Before
+    @BeforeEach
     public void setUp(){
-        MockitoAnnotations.initMocks(this);
-
+        MockitoAnnotations.openMocks(this);
         this.mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
 
-
     }
+
+    @Test
+    public void getAllRecords_success() throws Exception{
+
+        List<Book> records = new ArrayList<>(Arrays.asList(RECORD_1,RECORD_2,RECORD_3));
+
+        Mockito.when(bookRepository.findAll()).thenReturn(records);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get("/book")
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$",hasSize(3)));
+               // .andExpect(jsonPath("$[2].name", is("Grokking Algoritms")));
+    }
+
 }
---
+++ src/test/java/controller/BookControllerTest2.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookControllerTest2 {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllBooksRecords() {
+    }
+}
