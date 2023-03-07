0bc3c66b0e53d2f7353bfa8d0b7c46a92cb5eacd
Diego Salazar <dsalazar@ec.krugercorp.com>
2023-03-07 10:04:25 -0500

    [Update] Business Layer

--- src/main/java/controller/BookController.java
+++ src/main/java/controller/BookController.java
@@ -1,9 +1,14 @@
 package controller;
 
+import entity.Book;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import repository.BookRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/book")
@@ -12,6 +17,40 @@ public class BookController {
     @Autowired
     BookRepository repository;
 
+    @GetMapping
+    public List<Book> getAllBooksRecords(){
+        return repository.findAll();
+    }
+
+    @GetMapping(value = "{bookId}")
+    public Book getBookById(@PathVariable(value = "bookId") Long bookId){
+        return repository.findById(bookId).get();
+    }
+
+    @PostMapping
+    public Book createBookRecord(@RequestBody @Validated Book bookrecord){
+        return repository.save(bookrecord);
+    }
+
+    @PutMapping
+    public Book updateBookRecord(@RequestBody @Validated Book bookRecord) throws NotFoundException {
+        if(bookRecord == null || bookRecord.getBookId() == null){
+            throw new NotFoundException("BookRecord or ID must not be null");
+        }
+        Optional<Book> optionalBook = repository.findById(bookRecord.getBookId());
+        if(!optionalBook.isPresent()){
+            throw new NotFoundException("Book with ID: " + bookRecord.getBookId() + " does not exist");
+        }
+
+        Book existingBookRecord = optionalBook.get();
+        existingBookRecord.setName(bookRecord.getName());
+        existingBookRecord.setSummary(bookRecord.getSummary());
+        existingBookRecord.setRating(bookRecord.getRating());
+
+        return repository.save(existingBookRecord);
+    }
+
+    // TODO: Write /delete endpoint using TDD method
 
 
 }
